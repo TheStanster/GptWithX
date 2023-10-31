1f73f8152b8bdf2b9d971c2af6a2c7a8e64490ca
Stan Valster <stan.valster@gmail.com>
2023-10-31 23:00:20 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+// This is the build file for the project. It is written in Kotlin and is used to build the project.
+
+// Import system environment variables
+
+val OPENAI_TOKEN: String? = System.getenv("OPENAI_TOKEN")
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    application
+}
+
+group = "me.sval"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.xebia:xef-kotlin:0.0.3")
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("examples.MainKt")
+}
+
+kotlin{
+    jvmToolchain(17)
+}
